e1d9cd5187a22e94b0609a757ab2a7cb1ca24a89
Dereck Wonnacott <dereck@gmail.com>
2018-12-10 23:05:15 -0500

    Basic Logging roughly coded

---
+++ src/main/cpp/subsystem/Logging.cpp
@@ -0,0 +1,104 @@
+#include "subsystem/Logging.h"
+
+#include "Timer.h"
+#include "Preferences.h"
+
+Logging::Logging()
+{
+    Logging("./Logging/");
+}
+
+Logging::Logging(string path)
+{
+    Logging(path, "Log-#.csv");
+}
+
+Logging::Logging(string path, string _filename)
+{
+    // Get a log serial number from robot Preferences
+    auto pref = Preferences::GetInstance();
+    auto logNumber = pref->GetInt("LogNumber", 1);
+    auto logNumberStr = to_string(logNumber);
+
+    // pad with zeros
+    while (logNumberStr.length() < 6)
+    {
+        logNumberStr = "0" + logNumberStr;
+    }
+
+    // Replace # in the log file name with the log number
+    auto i = _filename.find("#");
+    if (i >= 0)
+    {
+        _filename = _filename.replace(i, logNumberStr.length(), logNumberStr);
+    }
+
+}
+
+void Logging::AddKey(string _key)
+{
+    // Cannot add new columns once logging is started
+    if (m_started)
+        return;
+
+    m_map[_key] = "";
+}
+
+void Logging::Start()
+{
+    // Cannot add new columns once logging is started
+    m_started = true;
+
+    // Lets open dat log file!
+    m_file.open(m_path + m_filename);
+
+    // Print the header
+    m_file << "Time, ";
+
+    // Print each log item
+    for (auto &i : m_map)
+    {
+        m_file << i.first << ", ";
+    }
+
+    // Force output to disk
+    m_file << endl;
+    m_file.flush();
+}
+
+void Logging::Log(string _key, string _value)
+{
+    // Must start log first
+    if (m_started)
+    {
+        return;
+    }
+
+    // Only update registered items
+    if (m_map.count(_key) == 0)
+    {
+        return;
+    }
+
+    m_map[_key] = _value;
+}
+
+void Logging::Commit()
+{
+    // Must start log first
+    if (m_started)
+    {
+        return;
+    }
+
+    m_file << frc::Timer::GetFPGATimestamp() << ", ";
+
+    for (auto &i : m_map)
+    {
+        m_file << i.second << ", ";
+    }
+
+    // Force output to disk
+    m_file << endl;
+    m_file.flush();
+}
---
+++ src/main/include/subsystem/Logging.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Logging
+{
+  private:
+    ofstream m_file;
+    map<string, string> m_map;
+
+    string m_path;
+    string m_filename;
+
+    bool m_started = false;
+
+  public:
+    Logging();
+    Logging(string path);
+    Logging(string path, string filename);
+    
+    void AddKey(string key);
+    void Start();
+    void Log(string key, string value);
+    void Commit();
+};
